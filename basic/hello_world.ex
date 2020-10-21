573ba9ec7901ea466dfd08204fec7e8b60f22084
Mauro F.J <mfjeng@gmail.com>
2020-10-21 15:49:58 -0300

    Added functions to "Hello World"

--- basic/hello_world.ex
+++ basic/hello_world.ex
@@ -1,10 +1,13 @@
 defmodule Hello do
   def print(lang \\ :pt)
-
   def print(:de), do: "Hallo Welt"
   def print(:en), do: "Hello World"
   def print(:es), do: "Hola Mundo"
   def print(:pt), do: "Olá Mundo"
   def print(:ru), do: "Привет мир"
+  def hello, do: "Olá Mundo"
 
+  def hi() do
+    IO.puts "Hello World"
+  end
 end
