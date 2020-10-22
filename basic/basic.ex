9a07fb5dd498fe7c1a5c3e5662fd70624406cbc7
mfjunior <maurofiorinjunior@gmail.com>
2020-10-22 14:46:40 -0300

    Studying math, comparison, logical operators, decision making and tuples.

--- basic/basic.ex
+++ basic/basic.ex
@@ -3,8 +3,8 @@ defmodule Basic do
   # gets user input and removes the newline
   def get_input do
     IO.gets("What is your name?")
-    |> String.trim
-    |> IO.puts   
+    |> String.trim()
+    |> IO.puts()
   end
 
   # data are immutable
@@ -76,4 +76,97 @@ defmodule Basic do
 
   end
 
+  def math_and_comparison do
+    IO.puts "5 + 4 = #{5 + 4}"
+    IO.puts "5 - 4 = #{5 - 4}"
+    IO.puts "5 * 4 = #{5 * 4}"
+    IO.puts "5 / 4 = #{5 / 4}"
+    IO.puts "5 div 4 = #{div(5, 4)}"
+    IO.puts "5 rem 4 = #{rem(5, 4)}"
+
+    IO.puts "4 == 4.0 : #{4 == 4.0}"
+    IO.puts "4 === 4.0 : #{4 === 4.0}"
+    IO.puts "4 != 4.0 : #{4 != 4.0}"
+    IO.puts "4 !== 4.0 : #{4 !== 4.0}"
+
+    IO.puts "5 > 4 : #{5 > 4}"
+    IO.puts "5 >= 4 : #{5 >= 4}"
+    IO.puts "5 < 4 : #{5 < 4}"
+    IO.puts "5 <= 4 : #{5 <= 4}"
+  end
+
+  def logical_operators do
+    age = 16
+    color = :red
+    IO.puts "Vote and drink: #{age >= 16 and color != :red}"
+    IO.puts "Vote && drink: #{age >= 16 && color != :red}"
+    IO.puts "Vote or drive: #{age >= 16 or age >= 18}"
+    IO.puts "Vote || drive: #{age >= 16 || age >= 18}"
+
+    IO.puts not true 
+    IO.puts !false   
+  end
+
+  def decision_making do
+    age = 16
+    if age >= 18 do
+      IO.puts "You can drive"
+    else
+      IO.puts "You can wait"
+    end
+
+    unless age === 18 do    ## !
+      IO.puts "You are not 18"
+    else
+      IO.puts "You are 18"
+    end
+
+    cond do
+      age >= 14 -> IO.puts "You can wait"
+      age >= 16 -> IO.puts "You can drive"
+      age >= 18 -> IO.puts "You can vote"
+      true -> IO.puts "Default"
+    end
+
+    case 2 do
+      1 -> IO.puts "Entered 1"
+      2 -> IO.puts "Entered 2"
+      _ -> IO.puts "Default"
+    end
+
+    IO.puts "Ternary: #{if age > 18, do: "Can Vote", else: "Can't Vote"}"
+
+  end
+
+  def tuples do
+    # create a tuple
+    my_stats = {190, 1.82, :Mauro}
+    IO.puts "Tuple #{is_tuple(my_stats)}"
+
+    # append value
+    my_stats2  = Tuple.append(my_stats, 40)
+    IO.inspect my_stats2
+    
+    # retrieve value by index
+    IO.puts "Age #{elem(my_stats2, 3)}"
+
+    # get tuple size
+    IO.puts "Size #{tuple_size(my_stats2)}"
+
+    # delete an index
+    my_stats3 = Tuple.delete_at(my_stats2, 0)
+    IO.inspect my_stats3
+    
+    # insert at an index
+    my_stats4 = Tuple.insert_at(my_stats3, 0, 1980)
+    IO.inspect my_stats4
+
+    # create a tuple with same values, n times
+    my_tuple_duplicated = Tuple.duplicate(0, 5)
+    IO.inspect my_tuple_duplicated
+
+    # tuples for pattern matching
+    {weight, height, name} = {190, 1.82, :Mauro}
+    IO.puts "Weight: #{weight}"
+  end
 end
