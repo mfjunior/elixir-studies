0e8959f001b9ccd806a7a42721c6e932122e81ab
mfjunior <maurofiorinjunior@gmail.com>
2020-10-24 18:03:18 -0300

    Added Stats module, which calculates recursively minimum and maximum of a list

---
+++ basic/stats.ex
@@ -0,0 +1,40 @@
+defmodule Stats do
+    
+  def minimum(list) do
+    [head | tail] = list
+    minimum(tail, head)      
+  end
+
+  defp minimum([], result) do
+    result
+  end
+
+  defp minimum([head | tail], result) when result < head do
+    minimum(tail, result)
+  end
+
+  defp minimum([head | tail], _result) do
+    minimum(tail, head)
+  end
+
+  def maximum(list) do
+      [head | tail] = list
+      maximum(tail, head)
+  end
+
+  defp maximum([], result) do
+    result
+  end
+
+  defp maximum([head | tail], result) when result > head do
+    maximum(tail, result)
+  end
+
+  defp maximum([head | tail], _result) do
+    maximum(tail, head)
+  end
+
+  def range(list) do
+    [minimum(list), maximum(list)]
+  end
+end
